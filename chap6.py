891da3aabf8d8e1daf82408a41acf54dcbe2027d
AJLeonardi <anthony.leonardi@gmail.com>
2013-02-04 19:08:27 -0500

    finished exercise 6.8

--- chap6.py
+++ chap6.py
@@ -24,6 +24,18 @@ print is_palindrome('heh')
 print is_palindrome('het')
 print is_palindrome('anthony')
 print is_palindrome('abcddcba')
+print is_palindrome('')
+print is_palindrome('a')
 
 
 # Ex 6.8
+def gcd(a, b):
+	if(b == 0):
+		return a
+	else:
+		return gcd(b, a%b)
+
+print gcd(1, 3)
+print gcd(5,3)
+print gcd(10,2)
+print gcd(5,15)
